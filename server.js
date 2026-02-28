9fbcb6bf319941a8d32382a7a2884255885617c3
Mohamed <ms7237322@gmail.com>
2026-02-28 07:59:45 +0200

    kanban-api

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const jsonServer = require('json-server');
 
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults({ static: false });
+const middlewares = jsonServer.defaults({ static: './public' });
+
+const fs = require('fs');
+if (!fs.existsSync('./public')) {
+    fs.mkdirSync('./public');
+}
 
 server.use(middlewares);
 server.use(router);
